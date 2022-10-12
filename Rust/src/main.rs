e68959726a9bbf7ede878367bd6c708c67cea83a
nordgaren <nordgarentv@gmail.com>
2022-10-11 19:14:54 -0700

    Finished Rust binary failed to commit in previous commit.

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -1,3 +1,79 @@
+use std::collections::HashMap;
+
 fn main() {
-    println!("Hello, world!");
+    let mut args = std::env::args().skip(1);
+
+    let arg = args.next().unwrap();
+    let mut database = Database::new("sample_rust.db").expect("Could not read database");
+
+    if arg == "set" {
+        let key = args.next().unwrap();
+        let value = args.next().unwrap();
+        println!("Added new entry: The key is '{}' and the value is '{}'", key, value);
+
+        database.insert(key, value);
+        database.save("sample_rust.db").expect("Database was unable to save.")
+
+    }
+
+    if arg == "get" {
+        let key = args.next().unwrap();
+        let value = database.get(&key);
+        println!("the value of '{}' is '{}'", key, value);
+    }
+
+}
+
+
+
+struct Database {
+    map: HashMap<String, String>
+}
+
+impl Database {
+    fn new (path: &str) -> Result<Database, std::io::Error> {
+        let contents = match std::fs::read_to_string(&path) {
+            Ok(string) => {
+                string
+            }
+            Err(error) => {
+                if error.kind() == std::io::ErrorKind::NotFound {
+                    std::fs::write(&path, "")?;
+                    std::fs::read_to_string(&path)?
+                } else {
+                    print!("{}",error);
+                    return Err(error)
+                }
+            }
+        };
+
+        let mut map = HashMap::new();
+
+        for line in contents.lines() {
+            let chunks = line.split_once('\t').unwrap();
+            let key = chunks.0;
+            let value = chunks.1;
+            map.insert(key.to_owned(), value.to_string());
+        }
+
+        Ok(Database { map })
+    }
+
+    fn insert(&mut self, key: String, value: String) {
+        self.map.insert(key.to_uppercase(), value.clone());
+        self.map.insert(key.to_owned(), value.to_owned());
+    }
+
+    fn get(&self, key: &str) -> &str {
+        self.map.get(key).expect("No result found for this key")
+    }
+
+    fn save(&self, path: &str) -> Result<(), std::io::Error>  {
+        let mut s = String::new();
+        for pair in &self.map {
+            s += &format!("{}\t{}\n", pair.0, pair.1);
+        }
+
+        std::fs::write(path, s)
+    }
 }
